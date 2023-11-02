b43a9be263321e9061dff024acd69e478cf38dae
JOHANN AMAYA LOPEZ <40411821+JohannBulls@users.noreply.github.com>
2023-11-02 18:20:00 -0500

    Update ConfigurationRepository.java

--- src/main/java/co/edu/escuelaing/cvds/lab7/repository/ConfigurationRepository.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/repository/ConfigurationRepository.java
@@ -2,12 +2,15 @@ package co.edu.escuelaing.cvds.lab7.repository;
 
 import co.edu.escuelaing.cvds.lab7.model.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ConfigurationRepository extends JpaRepository<Configuration, String> {
-    public List<Configuration> findByPropiedad(String propiedad);
+    List<Configuration> findByPropiedad(String propiedad);
 
+    @Query("SELECT c.completed, COUNT(c) FROM Configuration c GROUP BY c.completed")
+    List<Object[]> countConfigurationsGroupedByCompleted();
 }
